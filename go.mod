07d9e374ae32d6f070fd2a50d2bbc3e84f74dc89
Владимир Лепешко <stanlyzoolo@gmail.com>
2020-09-07 23:13:49 +0300

    Урок первый. 06.09.2020

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Stanlyzoolo/practiceGo
+
+go 1.15
+
+require github.com/sirupsen/logrus v1.6.0
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	// "fmt"
+	"github.com/sirupsen/logrus"
+)
+
+// func main() {
+// 	fmt.Println("Я умею запускать программы на go!")
+// }
+
+func main() {
+	logrus.Info("Я умею добавлять зависимости")
+}
