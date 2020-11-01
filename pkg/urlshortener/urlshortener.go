cc42d069daa7236844457a942deb2d62c3d6c3eb
Stanlyzoolo <stanlyzoolo@gmail.com>
2020-11-01 17:55:06 +0300

    Задание 5. Сокращатель ссылок

---
+++ pkg/urlshortener/urlshortener.go
@@ -0,0 +1,52 @@
+package urlshortener
+
+import (
+	"strconv"
+)
+
+type URLStore struct {
+	urls map[string]string
+}
+
+func NewURLStore() *URLStore {
+	return &URLStore{
+		urls: make(map[string]string)}
+}
+
+func (s *URLStore) Get(key string) string {
+	if url, ok := s.urls[key]; ok {
+		return url
+	}
+	return ""
+}
+
+func (s *URLStore) Set(key, url string) bool {
+	if _, ok := s.urls[key]; ok {
+		return false
+	}
+	s.urls[key] = url
+	return true
+}
+
+func (s *URLStore) Count() int {
+	return len(s.urls)
+}
+
+func (s *URLStore) Put(url string) string {
+	key := genKey(s.Count())
+	s.Set(key, url)
+	return key
+}
+
+func genKey(n int) string {
+	var shortURL string
+	if n%2 == 0 {
+		shortURL = "GoodKey №" + strconv.Itoa(n)
+		return shortURL
+	}
+	if n%2 != 0 {
+		shortURL = "PoorKey №" + strconv.Itoa(n)
+		return shortURL
+	}
+	return shortURL
+}
