d880004c8cb4046e6cf32102b63b2728a2b27540
Владимир Лепешко <stanlyzoolo@gmail.com>
2021-02-07 18:25:03 +0300

    Общие правки по заданиям + починил логи

--- cmd/main.go
+++ cmd/main.go
@@ -8,19 +8,19 @@ import (
 	"github.com/Stanlyzoolo/practiceGo/pkg/storage"
 	"github.com/Stanlyzoolo/practiceGo/pkg/usecases"
 
-	"github.com/kelseyhightower/envconfig"
+	// "github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
 )
 
-type EnvConfig struct {
-	LogFile string `envconfig:"LOG_FILE"`
-}
+// type EnvConfig struct {
+	// LogFile string `envconfig:"LOG_FILE"`
+// }
 
 func main() {
-	var eConf EnvConfig
-	envconfig.Process("", &eConf)
+	// var eConf EnvConfig
+	// envconfig.Process("", &eConf)
 
-	file, _ := os.OpenFile(eConf.LogFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	file, _ := os.OpenFile("practice.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 
 	var log = logrus.New()
 	log.Out = file
@@ -30,14 +30,13 @@ func main() {
 		Samples: []usecases.Fahrenheit{},
 	}
 
-	usecases.CheckAndSave(tempHistoryHandler, usecases.Celsius(32), 5)
+	usecases.CheckAndSave(tempHistoryHandler, usecases.Celsius(30), 1)
 
 	datatypes.CreateQueueObj(tempHistoryHandler)
-
+	datatypes.CreateListNodeObj(tempHistoryHandler)
+	
 	word := "Lenovo"
-
-	datatypes.ReverseWordOne(word, tempHistoryHandler)
-	datatypes.ReverseWordTwo(word, tempHistoryHandler)
+	datatypes.ReverseWord(word, tempHistoryHandler)
 
 	sent := "Lenovo is good"
 	datatypes.ReverseWordsinSentence(sent, tempHistoryHandler)
